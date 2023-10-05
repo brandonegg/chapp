b6720dee8180c98e0f7c633eff4dd4646d82d94d
Brandon Egger <brandonegger64@gmail.com>
2023-10-04 20:53:25 -0500

    add readline output demo

--- operating-systems/hw4-memory_api/task1/task1.c
+++ operating-systems/hw4-memory_api/task1/task1.c
@@ -1,40 +1,46 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/mman.h>
+#include <stdio.h>
+#include <math.h>
 
-// A function that checks if a given address is in the virtual address space of the program
-int is_valid_address(void *addr) {
-  // Get the page size of the system
-  long page_size = sysconf(_SC_PAGESIZE);
-  // Align the address to the page boundary
-  void *page_start = (void *)((unsigned long)addr & ~(page_size - 1));
-  // Use mincore to check if the page is mapped
-  unsigned char vec;
-  if (mincore(page_start, page_size, &vec) == -1) {
-    // The page is not mapped
-    return 0;
+void read_file(char* const fileName) {
+  FILE* file = fopen(fileName, "r"); /* should check the result */
+  char line[256];
+
+  while (fgets(line, sizeof(line), file)) {
+      /* note that fgets don't strip the terminating \n, checking its
+          presence would allow to handle lines longer that sizeof(line) */
+      printf("%s", line); 
   }
-  // The page is mapped
-  return 1;
+  /* may check feof here to make a difference between eof and io failure -- network
+      timeout for instance */
+
+  fclose(file);
 }
 
-int main(int argc, char *argv[]) {
+/**
+ * @returns 1 if address provided is in the virtual memory otherwise 0
+ */
+int is_address_in_virt_mem(void *addr) {
+  int pid = (int) getpid();
+
+  char fileName[50];
+  sprintf(fileName, "/proc/%d/maps", pid);
+
+  read_file(fileName);
+
+  return 0;
+}
+
+int main(int argc, char *argv[], char* env[]) {
   if (argc != 2) {
     fprintf(stderr, "Usage: %s <required_address>\n", argv[0]);
     exit(-1);
   }
-  // Convert the argument to a pointer
-  void *addr = (void *)strtoul(argv[1], NULL, 0);
-  // Check if the address is valid
-  if (is_valid_address(addr)) {
-    // Print the value of the byte at the address
-    printf("Is valid");
-    printf("%d\n", *(unsigned char *)addr);
-    exit(0);
-  }
-  else {
-    // Do nothing and exit with -1
-    exit(-1);
-  }
+
+  char* addr = (char *)strtoul(argv[1], NULL, 16);
+
+  int result = is_address_in_virt_mem(addr);
+
+  exit(-1);
 }
