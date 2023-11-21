97303d80e3d2193e0884e91a4c89e41674132fde
Blake Hohmann <blake-hohmann@uiowa.edu>
2023-11-21 01:28:32 -0600

    chore: previous commit was missing server changes

--- chat_app/server.py
+++ chat_app/server.py
@@ -75,6 +75,7 @@ class ChatServer():
             try:
                 data = client_socket.recv(1024).decode()
                 if not data:
+                    
                     continue
 
                 try:
